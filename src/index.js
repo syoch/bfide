c0fbb2d423100c27f0b735668d7a95973fd532c5
syoch <hacksyoch@gmail.com>
2020-10-18 16:59:27 +0900

    アクション内の要素でサイズ指定がうまくいってない不具合を修正

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@
 /// <reference path="../typings/tjq.d.ts" />
 
 $(function () {
+    let elems = $(".action").children();
+    for (let i = 0; i < elems.length; i++) {
+        const elem = $(elems[i]);
+        elem.css("--size", elem.attr("size"));
+    }
 
     $("#btn_run").on("click", () => {
         console.log("run", $("#editor").val());
